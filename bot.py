06036d97cc35f97d2cbacfebbc76698199ebc574
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2024-08-28 18:32:26 +0400

    new

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ async def start(update: Update, _: CallbackContext) -> None:
             await user_collection.insert_one({
                 "user_id": user_id,
                 "status": "inactive",
-                "date": '{datetime.date()}/{datetime.month()}/{datetime.year()}'
+                "date": '{datetime.year()}-{datetime.month()}-{datetime.date()}'
             })
             logger.info(f"Added user {user_id} to the database with status 'inactive'.")
 
